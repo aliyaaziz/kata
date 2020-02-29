6cf97173994c40e4246c42a45512f09c4a88850a
Aliya Aziz <aliyaaziz@apple.com>
2020-02-29 15:36:26 +0000

    Working on pricing rules methods

--- Supermarket/PricingRules.cs
+++ Supermarket/PricingRules.cs
@@ -1,18 +1,32 @@
 using System;
+using System.Collections.Generic;
 using Supermarket.Interfaces;
 
 namespace Supermarket
 {
     public class PricingRules : IPricingRules
     {
+        Dictionary<string, SortedDictionary<int, int>> _rules;
+        public PricingRules()
+        {
+            _rules = new Dictionary<string, SortedDictionary<int, int>>();
+        }
         public void Add(string item, int count, int price)
         {
-            throw new System.NotImplementedException();
+            if(_rules.ContainsKey(item))
+            {
+                var itemRules = _rules[item];
+                itemRules.Add(count, price);
+            }
+            else{
+                _rules.Add(item, new SortedDictionary<int, int>(){{count, price}});
+            }
         }
 
         public int GetPrice(string item, int scanned)
         {
-            throw new System.NotImplementedException();
+            //TODO implement
+            return 0;
         }
     }
 }
